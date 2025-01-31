a3f120c9f5e7870208ba78da088369c0225e43c2
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-01-31 22:07:55 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+
+global. creator = "Fajar Official"
+
+global.loghandler = {
+  error: {
+        status: false,
+        creator: `${creator}`,
+        message: 'mungkin sedang dilakukan perbaikan'
+    }
+}
