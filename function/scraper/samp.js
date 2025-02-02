855cf57334d35366dcbdb4392a418a413a3d4598
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-02 20:32:09 +0700

    Add files via upload

---
+++ function/scraper/samp.js
@@ -0,0 +1,24 @@
+const sampQuery = require('samp-query');
+
+async function getServerStatus(ip, port) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: ip,
+            port: parseInt(port, 10), // Pastikan port dalam bentuk angka
+            timeout: 1000
+        };
+
+        sampQuery(options, (error, response) => {
+            if (error) {
+                resolve(`Server sedang offline atau tidak merespons.`);
+            } else {
+                const serverStatus = `
+IP Server : ${options.host}:${options.port} Nama Server : ${response.hostname} Pemain Online : ${response.online} Max Pemain : ${response.maxplayers} GameMode : ${response.gamemode} Map : ${response.mapname} Version : ${response.rules.version} Weather : ${response.rules.weather} Url : ${response.rules.weburl} Time :  ${response.rules.worldtime} Player : ${response.players} Status : Online ✅
+`;
+                resolve(serverStatus);
+            }
+        });
+    });
+}
+
+module.exports = getServerStatus
