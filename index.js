d150f878ff5d27c6bd4a5514d2c2185d4c8f09af
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-02 09:01:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,371 @@
+require('./settings.js')
+var express = require("express"), cors = require("cors"), secure = require("ssl-express-www");
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const ptz = require('./function/index') 
+const axios = require('axios')
+const isUrl = require("is-url")
+const TikTokScraper = require('tiktok-scraper');
+
+function getBaseUrl(req) {
+    return `${req.protocol}://${req.get('host')}`;
+}
+
+var app = express();
+app.enable("trust proxy");
+app.set("json spaces", 2);
+app.use(cors());
+app.use(secure);
+const port = 3000;
+
+
+app.get('/stats', (req, res) => {
+  const stats = {
+    platform: os.platform(),
+    architecture: os.arch(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    uptime: os.uptime(),
+    cpuModel: os.cpus()[0].model,
+    numCores: os.cpus().length,
+    loadAverage: os.loadavg(),
+    hostname: os.hostname(),
+    networkInterfaces: os.networkInterfaces(),
+    osType: os.type(),
+    osRelease: os.release(),
+    userInfo: os.userInfo(),
+    processId: process.pid,
+    nodeVersion: process.version,
+    execPath: process.execPath,
+    cwd: process.cwd(),
+    memoryUsage: process.memoryUsage()
+  };
+  res.json(stats);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
+
+app.get('/wl', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'whitelist.html'));
+});
+
+
+app.get('/api/ragbot', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ptz.ragBot(message);
+    res.status(200).json({
+      status: 200,
+      creator: "Fajar Official",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/api/islam/niatmagrib', async (req, res, next) => {
+    var apikey = req.query.apikey
+    var text = req.query.page
+    
+    fetch(encodeURI(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/master/data/NiatMaghrib.json`))
+        .then(response => response.json())
+        .then(data => {
+            var result = data;
+            res.json({
+                result
+            })
+        })
+        .catch(e => {
+            console.log(e);
+            res.json(loghandler.error)
+        })
+});
+// Endpoint untuk degreeGuru
+app.get('/api/degreeguru', async (req, res) => {
+  try {
+    const { message }= req.query;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ptz.degreeGuru(message);
+    res.status(200).json({
+      status: 200,
+      creator: "Fajar Official",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk smartContract
+app.get('/api/smartcontract', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ptz.smartContract(message);
+    res.status(200).json({
+      status: 200,
+      creator: "Fajar Official",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk blackboxAIChat
+app.get('/api/blackboxAIChat', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ptz.blackboxAIChat(message);
+    res.status(200).json({
+      status: 200,
+      creator: "Fajar Official",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/api/tiktok", async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.status(400).json({ error: "URL is required." });
+
+  try {
+    const { tiktokdl } = require("tiktokdl");
+    const data = await tiktokdl(url);
+    if (!data) return res.status(404).json({ error: "No data found." });
+    res.json({ status: true, creator: "Fajar Official", result: data });
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+app.get('/api/islam/niatisya', async (req, res) => {
+    var text = req.query.page
+    
+    fetch(encodeURI(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/refs/heads/master/data/NiatIsya.json`))
+        .then(response => response.json())
+        .then(data => {
+            var result = data;
+            res.json({
+                result
+            })
+        })
+        .catch(e => {
+            console.log(e);
+            res.json(loghandler.error)
+        })
+});
+
+app.get('/api/islam/niatashar', async (req, res) => {
+    
+    fetch(encodeURI(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/master/data/NiatIsya.json`))
+        .then(response => response.json())
+        .then(data => {
+            var result = data;
+            res.json({
+                result
+            })
+        })
+        .catch(e => {
+            console.log(e);
+            res.json(loghandler.error)
+        })
+});
+
+app.get('/api/islam/niatsubuh', async (req, res) => {
+    
+    fetch(encodeURI(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/master/data/NiatShubuh.json`))
+        .then(response => response.json())
+        .then(data => {
+            var result = data;
+            res.json({
+                result
+            })
+        })
+        .catch(e => {
+            console.log(e);
+            res.json(loghandler.error)
+        })
+});
+
+app.get('/api/islam/niatzuhur', async (req, res) => {
+    
+    fetch(encodeURI(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/master/data/NiatDzuhur.json`))
+        .then(response => response.json())
+        .then(data => {
+            var result = data;
+            res.json({
+                result
+            })
+        })
+        .catch(e => {
+            console.log(e);
+            res.json(loghandler.error)
+        })
+});
+
+app.get('/api/islam/niatshalat', async (req, res) => {
+    var text = req.query.page
+    
+    fetch(encodeURI(`https://raw.githubusercontent.com/zeeoneofficial/My-SQL-Results/master/data/dataNiatShalat.json`))
+        .then(response => response.json())
+        .then(data => {
+            var result = data;
+            res.json({
+                result
+            })
+        })
+        .catch(e => {
+            console.log(e);
+            res.json(loghandler.error)
+        })
+});
+
+app.get("/api/translate", async (req, res) => {
+  const text = req.query.text;
+  if (!text) return res.status(400).json({ error: "Text is required." });
+
+  try {
+    const response = await axios.get(`https://api.siputzx.my.id/api/tools/translate`, {
+      params: { text: text, source: "auto", target: "id" }
+    });
+    res.json({ status: true, creator: "Fajar Official", result: response.data.translatedText });
+  } catch {
+    res.status(500).json({ error: "An error occurred while processing the translation." });
+  }
+});
+
+app.get("/api/ytmp3", async (req, res) => {
+    const url = req.query.url;
+    if (!url) return res.status(400).json({ error: "Url is required." });
+
+    try {
+        const response = await axios.get(`https://api.siputzx.my.id/api/d/youtube?q=${url}`);
+        const data = response.data;
+
+        res.json({
+            status: true,
+            creator: "Fajar Official",
+            result: {
+                Judul: data.data.title,
+                thumbnail: data.data.thumbnailUrl,
+                durasi: data.data.duration,
+                UrlDownload: data.data.sounds
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while fetching data." });
+    }
+});
+
+app.get('/api/tiktok/stalk', async (req, res, next) => {
+        username = req.query.username
+
+    if (!username) return res.json(loghandler.notusername)
+
+
+    TikTokScraper.getUserProfileInfo(username)
+        .then(user => {
+            res.json({
+                status : true,
+                creator : `${creator}`,
+                result : user
+            })
+        })
+        .catch(e => {
+             res.json({
+                 status : false,
+                 creator : `${creator}`,
+                 message : "error, mungkin username anda tidak valid"
+             })
+         })
+});
+
+
+app.get('/api/stalker/freefire', async (req, res) => {
+    try {
+        var id = req.query.id;
+
+        if (!id) return res.json({ status: false, message: 'Parameter ID tidak ditemukan' });
+
+        let response = await fetch(`http://localhost:3000/api/stalker/freefire?id=${id}`);
+        let anu = await response.json();
+
+        if (!anu.result) return res.json({ status: false, message: 'Data tidak ditemukan' });
+
+        res.json({
+            status: true,
+            creator: creator,
+            result: anu.result
+        });
+    } catch (error) {
+        console.error(error);
+        res.json({ status: false, message: 'Terjadi kesalahan dalam server' });
+    }
+});
+
+app.get("/api/gpt", async (req, res) => {
+const text = req.query.text;
+
+if (!text) {
+return res.status(400).send("Parameter 'text' is required.");
+}
+
+
+try {
+const requestData = {
+operation: "chatExecute",
+params: {
+text: text,
+languageId: "6094f9b4addddd000c04c94b",
+toneId: "60572a649bdd4272b8fe358c",
+voiceId: ""
+}
+};
+
+const config = {
+headers: {
+Accept: "application/json, text/plain, */*",
+Authentication: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTZjMjFhMGE1NTNiNjE1MDhmNWIxOSIsImlhdCI6MTcxMjc2NzUxNH0.qseE0iNl-4bZrpQoB-zxVsc-pz13l3JOKkg4u6Y08OY",
+"Content-Type": "application/json"
+}
+};
+let {data} = await axios.post("https://api.rytr.me/", requestData, config)
+data.data.content = data.data.content.replace(/<\/?p[^>]*>/g, '');
+res.json(data);
+} catch (error) {
+console.error(error);
+res.status(500).send("Internal Server Error");
+}
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).send("Halaman tidak ditemukan");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Ada kesalahan pada server');
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
