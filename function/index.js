f7de8d4ebf7a990cbb93d4b4644ee4a5e230832c
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-04 09:16:15 +0700

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   getServerStatus: require('./scraper/samp'),
   capcutdl: require('./scraper/capcutdl'),
-  tiktokStalk: require('./scraper/ttstalk')
+  tiktokStalk: require('./scraper/ttstalk'),
+  instanav: require('./scraper/igdl')
 }
