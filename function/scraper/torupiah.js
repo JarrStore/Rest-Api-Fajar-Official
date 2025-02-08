cc00dcce765fbe51062b346b3cd052bbfba20d20
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-08 11:18:27 +0700

    Create torupiah.js

---
+++ function/scraper/torupiah.js
@@ -0,0 +1,10 @@
+function toRupiah(angka) {
+  var saldo = '';
+  var angkarev = angka.toString().split('').reverse().join('');
+  
+  for (var i = 0; i < angkarev.length; i++) {
+    if (i % 3 == 0 && i !== 0) saldo += '.';  // Add dot after every 3 digits, except at the start
+    saldo += angkarev[i];
+  }
+  return '' + saldo.split('').reverse().join('');
+}
