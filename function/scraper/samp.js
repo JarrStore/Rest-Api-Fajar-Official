bf85f0356e562565ad294b750d4e13c539cda500
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-08 18:03:50 +0700

    Update samp.js

--- function/scraper/samp.js
+++ function/scraper/samp.js
@@ -8,39 +8,30 @@ async function getServerStatus(ip, port) {
             timeout: 3000
         };
 
-        // Menjalankan query ke server SAMP
         SampQuery(options, (error, response) => {
             if (error) {
                 console.error("Error detail:", error);
-                return reject("Terjadi kesalahan saat menghubungi server.");
+                return reject(null);
             }
 
-            // Pastikan response memiliki data sebelum mengaksesnya
             if (!response) {
-                return reject("Tidak ada respons dari server.");
+                return reject(null);
             }
 
-            // Ambil data dari response
-            const serverIP = options.host;
-            const serverPort = options.port;
-            const gamemode = response.gamemode || "Tidak diketahui";
-            const playerOnline = response.online || 0;
-            const maxPlayers = response.maxplayers || 0;
-            const hostname = response.hostname || "Tidak diketahui";
-            const lagCompensation = response.rules?.lagcomp || "Tidak diketahui";
-            const mapName = response.mapname || "Tidak diketahui";
-            const version = response.rules?.version || "Tidak diketahui";
-            const weather = response.rules?.weather || "Tidak diketahui";
-            const webUrl = response.rules?.weburl || "Tidak diketahui";
-            const worldTime = response.rules?.worldtime || "Tidak diketahui";
-
-            // Ambil daftar pemain jika ada
-            const players = response.players?.map(player => player.name).join(", ") || "Tidak ada pemain";
-
-            // Gabungkan semua informasi
-            const serverStatus = `IPServer: ${serverIP}:${serverPort} NamaServer: ${hostname} PemainOnline: ${playerOnline} MaxPemain: ${maxPlayers} GameMode: ${gamemode} Map: ${mapName} Version: ${version} Weather: ${weather} Url: ${webUrl} Time: ${worldTime} Player: ${players}`;
-
-            resolve(serverStatus);
+            resolve({
+                ip: options.host,
+                port: options.port,
+                hostname: response.hostname || "Tidak diketahui",
+                players_online: response.online || 0,
+                max_players: response.maxplayers || 0,
+                gamemode: response.gamemode || "Tidak diketahui",
+                map_name: response.mapname || "Tidak diketahui",
+                version: response.rules?.version || "Tidak diketahui",
+                weather: response.rules?.weather || "Tidak diketahui",
+                web_url: response.rules?.weburl || "Tidak diketahui",
+                world_time: response.rules?.worldtime || "Tidak diketahui",
+                players: response.players?.map(player => player.name) || []
+            });
         });
     });
 }
