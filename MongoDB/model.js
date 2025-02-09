39f24b933a2ad13c36a0427a56a3786b536114a4
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-09 21:07:07 +0700

    Update model.js

--- MongoDB/model.js
+++ MongoDB/model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Users = mongoose.Schema({
-    apikey: { type: String },
+    apikey: { type: String }
 }, { versionKey: false });
+
 module.exports.User = mongoose.model('api', Users);
