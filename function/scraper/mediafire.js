a1f5b0f98a619ef4c73bbc7d116b0a39075e8b47
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-09 16:15:20 +0700

    Update mediafire.js

--- function/scraper/mediafire.js
+++ function/scraper/mediafire.js
@@ -6,21 +6,32 @@ async function mediafire(query) {
     axios.get(query)
       .then(({ data }) => {
         const $ = cheerio.load(data);
-        const judul = $('body > div.mf-dlr.page.ads-alternate > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').text();
-        const size = $('body > div.mf-dlr.page.ads-alternate > div.content > div.center > div > div.dl-info > ul > li:nth-child(1) > span').text();
-        const upload_date = $('body > div.mf-dlr.page.ads-alternate > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text();
+        
+        // Check if we can find the download link and other necessary details
+        const judul = $('body > div.mf-dlr.page.ads-alternate > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').text().trim();
+        const size = $('body > div.mf-dlr.page.ads-alternate > div.content > div.center > div > div.dl-info > ul > li:nth-child(1) > span').text().trim();
+        const upload_date = $('body > div.mf-dlr.page.ads-alternate > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text().trim();
         const link = $('#downloadButton').attr('href');
         
+        // If we can't find the required elements, reject the promise with an error message
+        if (!judul || !size || !upload_date || !link) {
+          return reject(new Error('Missing data or incorrect structure'));
+        }
+
         const result = {
-          judul: link.split('/')[5],
+          judul: judul,
           upload_date: upload_date,
           size: size,
           mime: link.split('/')[5].split('.')[1],
           link: link
         };
+        
         resolve(result);
       })
-      .catch(reject);
+      .catch(error => {
+        console.error('Error during scraping:', error);  // Log the error for debugging
+        reject(new Error('Failed to scrape MediaFire page'));
+      });
   });
 }
 
