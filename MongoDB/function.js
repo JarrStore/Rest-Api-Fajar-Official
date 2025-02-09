0e4fc9c9e19b08305e4a11eb3b956880a6063460
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-09 21:36:14 +0700

    Delete MongoDB directory

--- MongoDB/function.js
+++
@@ -1,6 +0,0 @@
-const { User } = require('./model');
-
-async function cekKey(apikey) {
-    let db = await User.findOne({ apikey: apikey });
-    return db ? db.apikey : false;
-}
--- MongoDB/model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const Users = mongoose.Schema({
-    apikey: { type: String }
-}, { versionKey: false });
-
-module.exports.User = mongoose.model('api', Users);
