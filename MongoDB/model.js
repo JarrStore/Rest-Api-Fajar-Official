33d2d851dbe1c103f31358a5c92bd4e7b0bc0551
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-09 20:30:08 +0700

    Create model.js

---
+++ MongoDB/model.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+const Users = mongoose.Schema({
+    apikey: { type: String },
+}, { versionKey: false });
+module.exports.User = mongoose.model('api', Users);
