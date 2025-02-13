c6471a64553e72c76f63783b4a06febbe5208693
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-13 20:19:32 +0700

    Update connect.js

--- MongoDB/connect.js
+++ MongoDB/connect.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://fajarshidik709:5xDc3VO36uCBZq5N@cluster0.qqz27.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGODB_URI = "mongodb+srv://fajarshidik709:5xDc3VO36uCBZq5N@cluster0.qqz27.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+function connectMongoDb() {
+    mongoose.connect(MONGODB_URI, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true
+    });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+      console.log('[INFO] Connect to DB success!');
+    });
+};
 
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.error("MongoDB Connection Error:", err));
-
-module.exports = mongoose;
+module.exports.connectMongoDb = connectMongoDb
