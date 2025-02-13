78ad19b01fcfaa9641baf8503d211c7d2b604086
JarrStore <163243032+JarrStore@users.noreply.github.com>
2025-02-13 20:34:09 +0700

    Update schema.js

--- MongoDB/schema.js
+++ MongoDB/schema.js
@@ -1,10 +1,10 @@
-const mongoose = require('./connect'); // Menggunakan koneksi dari connect.js
+const mongoose = require('mongoose'); // Menggunakan koneksi dari connect.js
 
-const statsSchema = new mongoose.Schema({
+const statsSchema = new mongoose.Schema({ // Pakai `new mongoose.Schema`
     totalRequests: { type: Number, default: 0 },
     totalPenggunaApikey: { type: Number, default: 0 }
 }, { versionKey: false });
 
-const Stats = mongoose.model('Stats', statsSchema);
+const Stats = mongoose.model('Stats', statsSchema); // Model harus dibuat dengan mongoose.model
 
 module.exports = Stats;
